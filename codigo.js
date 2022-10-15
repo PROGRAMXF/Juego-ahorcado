7bd5cad4fc1ff91a2dd863e992d1436c3b7713e2
Fernando <fh1273509@gmail.com>
2022-10-14 23:16:33 -0300

    arreglando el punto anterior

--- codigo.js
+++ codigo.js
@@ -1,6 +1,8 @@
 let palabrita;
 let cantidad_errores = 0; //cuantas veces me equivoque
 let cantidad_aciertos = 0; //cuantas veces acerte
+
+
 const palabras = [
   //indices del array:
   "naranja", //0
@@ -17,19 +19,22 @@ const palabras = [
   "aviones", //11
 ];
 
+const btn = id("jugar");
 const imagen = id( 'imagen' );
-const btn = document.getElementById("jugar");
-btn.addEventListener("click", iniciar); //iniciar es la funcion que hacemos referencia abajo
+const btn_letras = document.querySelectorAll("#letras");
+
+//con el click se inicia el juego
+btn.addEventListener('click', iniciar); //iniciar es la funcion que hacemos referencia abajo
+
 
 function iniciar(event) {
   imagen.src = 'img/img0.jpg';
   btn.disabled = true; //el boton no se puede tocar de vuelta
-
   cantidad_errores = 0; //cuantas veces me equivoque
   cantidad_aciertos = 0; //cuantas veces acerte
 
-  const parrafo = id("adivina_la_palabra");
-  parrafo.innerHTML = ""; //me vacia los span para que no se acumulen cuando hago click
+  const parrafo = id('adivina_la_palabra');
+  parrafo.innerHTML = ''; //me vacia los span para que no se acumulen cuando hago click
 
   const cantidad_palabras = palabras.length; // 1 - cuando aprete adivinar palabra va a sacar un valor al azar
   //variables que nos cuenta la cantidad de palabras del array
@@ -40,15 +45,19 @@ function iniciar(event) {
   console.log(palabrita); //3 - me va a mostrar por consola la palabrita que tengo que adivinar
   const cant_letras = palabrita.length; //con el punto length me doy cuenta y creo la cantidad de caracteres de palabrita, osea
   // de los span de cada palabra.
+  for (let i = 0; i < btn_letras.length; i++) {
+    // funcion que recorre letra por letra
+    btn_letras[i].disabled = false;
+  }
 
   for (let i = 0; i < cant_letras; i++) {
-    const span = document.createElement("span"); // 4 - creamos una etiqueta span vacia por cada letra que tenga la palabra
+    const span = document.createElement('span'); // 4 - creamos una etiqueta span vacia por cada letra que tenga la palabra
     parrafo.appendChild(span); //al parrafo le agregamos un span
   }
 }
 
-const btn_letras = document.querySelectorAll("#letras");
 
+//click para adivinar letra
 for (let i = 0; i < btn_letras.length; i++) {
   // funcion que recorre letra por letra
   btn_letras[i].addEventListener("click", clickear_letras);
@@ -58,7 +67,8 @@ function clickear_letras(event) {
   // el event lo necesito para saber que letra toque para llamar a la funcion
   const spans = document.querySelectorAll("#adivina_la_palabra span");
   const button = event.target;
-  button.disabled = true; //una vez que toque la letra el boton de la letra se desabilita
+  button.disabled = true; //una vez que toque la letra el boton de la letra se desabilita  
+  
   const letra = button.innerHTML.toLowerCase(); //va cambiando la letra entre span y span
   const palabra = palabrita.toLowerCase(); //me nivela las letras todas a minuscula
 
@@ -79,19 +89,44 @@ function clickear_letras(event) {
       //que mostrarle esta letra
       spans[i].innerHTML = letra;
       cantidad_aciertos++;
-
       acerto = true;
+      }
     }
     if (acerto == false) {
       //si la variable acerto esta en falso quiere decir que la letra no existe
       cantidad_errores++; //entonces me suma un error por que no existe la letra
-      const source = `img/img${cantidad_errores}.jpg`; //va a la carpeta de imagenes y busca la imagen
+      const source = `img/img${cantidad_errores}.jpg` ; //va a la carpeta de imagenes y busca la imagen
       // const imagen = id("imagen"); //buscame la foto
       imagen.src = source; //y cambiame el source
     }
-  }
+    if(cantidad_errores == 7){
+      id('resultado').innerHTML = "Perdiste. La palabra era: "  + palabrita;
+      game_over();
+      
+    }
+    else if(cantidad_aciertos == palabrita.length){      
+      id('resultado').innerHTML = "GANASTE!!!!";
 
-  console.log(
-    "la letra " + letra + " en la palabra " + palabra + " existe?:" + acerto
-  );
-}
+      //creo una variable para mostrar el gif cuando gano
+      var win = document.getElementById('imagen')
+      win.src = "img/win.gif";
+      game_over();
+
+      
+    }
+    console.log("la letra " + letra + " en la palabra " + palabra + " existe?:" + acerto);
+  
+    }
+    //fin del juego
+    function game_over(){
+      for (let i = 0; i < btn_letras.length; i++) {
+        // funcion que recorre letra por letra
+        btn_letras[i].disabled = true;
+      }
+      btn.disabled = false;
+    }
+    game_over();
+
+
+
+
